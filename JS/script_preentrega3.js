e39b8c57fb135082eb5a676e20440ab8488b2d29
Gonzalo2194 <gonzalosoto.2194@gmail.com>
2023-10-05 06:08:57 -0300

    se agrega funcion carrito

--- JS/script_preentrega3.js
+++ JS/script_preentrega3.js
@@ -13,8 +13,11 @@ class Basedatos {
     constructor(){
         this.productos = [];
         this.agregarRegistro(1,"hamburguesa con queso", 150,"burgers","pexels-oleksandr-p-12932961 (1).jpg");
-        this.agregarRegistro(2,"prueba", 190,"test1","pexels-oleksandr-p-12932961 (1).jpg")
-        this.agregarRegistro(2,"prueba2", 250,"test2","pexels-oleksandr-p-12932961 (1).jpg")
+        this.agregarRegistro(2,"Empanada", 190,"empanadas","empanadas.jpg")
+        this.agregarRegistro(3,"Coca Cola", 50,"refrescos","gaseosa.jpg")
+        this.agregarRegistro(4,"Pizza", 200,"Pizzas","pizza.jpg")
+        this.agregarRegistro(5,"Papas fritas", 220,"fritas","fritas.jpg")
+        this.agregarRegistro(6,"Tiramisú", 150,"postres","tiramisu.jpg")
     }
 
     agregarRegistro(id, nombre ,precio, categoria, imagen){
@@ -24,22 +27,89 @@ class Basedatos {
     }
     TraerRegistros() {
         return this.productos;
-    }
+    } 
     
-    RegistrosPorId (id) {
-        return this.productos.find ((producto)== producto.id === id);
+    registrosPorId (id) {
+        return this.productos.find ((producto)=> producto.id === id);
     
     }
     
     RegistrosPorNombre (palabra){
-        return this.productos.filter ((producto)== producto.nombre.toLowerCase(). includes(palabra.toLowerCase()));
+        return this.productos.filter ((producto)=> producto.nombre.toLowerCase(). includes(palabra.toLowerCase()));
     
     }
 }
 
+class Carrito{
+    constructor(){
+        this.carrito = [];
+        this.total = 0;
+        this.cantidadProductos = 0;
+    }
+    estaEnCarrito({ id }){
+        return this.carrito.find((producto) => producto.id == id);
+    }
+
+    agregar(producto){
+        const productoEncarrito = this.estaEnCarrito(producto);
+
+        if(productoEncarrito){
+            this.carrito.push({...producto,cantidad: 1 });
+
+        } else {
+            productoEnCarrito.cantidad++;  
+        }
+        this.listar();
+    }
+
+    quitar(id){
+        const indice = this.carrito.findIndex((producto) => producto.id == id);
+        if(this.carrito[indice].cantidad > 1 ){
+            this.carrito [indice].cantidad--;
+        } else{
+        this.carrito.splice (indice, 1);
+        }
+        this.listar();
+    }
+
+    listar(){
+        this.total = 0;
+        this.cantidadProductos = 0;
+        divCarrito.innerHTML = "";
+
+        for (const producto of this.carrito){
+            divCarrito.innerHTML += `
+            <div class="productoCarrito">
+                <h2>${producto.nombre} </h2> 
+                <p>$${producto.precio} </p>
+                <p>Cantidad: ${producto.cantidad}</p>
+                <a>href="#" class= "btnQuitar" data-id="${producto.id}">Quitar del carrito"</a>
+            </div>
+            `;
+            this.total += producto.precio * producto.cantidad;
+            this.cantidadProductos += producto.cantidad;
+        }
+
+        const botonesQuitar = document.querySelectorAll(".btnQuitar");
+        for (const boton of botonesQuitar);
+            boton.addEventListener("click",(Event)=>{
+                Event.preventDefault();
+                const idProducto = Number(boton.dataset.id);
+                this.quitar(idProducto);
+            })
+    }
+}
+
+
+
+
+
 const bd = new Basedatos();
 
+const carrito = new Carrito();
+
 const divProductos = document.querySelector("#productos");
+const divCarrito = document.querySelector ("#carrito");
 
 cargarProductos(bd.TraerRegistros());
 
@@ -50,7 +120,7 @@ function cargarProductos(productos) {
         <section class="container-fluid row mt-5 ">
             <div class="">
                 <h2>${producto.nombre}</h2>
-                <p class= "letrapedido">${producto.precio}</p>
+                <p class= "letrapedido">$${producto.precio}</p>
                 <div class= " img bordes">
                     <img class ="img" src="asset/${producto.imagen}"< />
                 </div>
@@ -67,8 +137,10 @@ function cargarProductos(productos) {
         boton.addEventListener("click", (event) => {
             event.preventDefault();
             const idProducto = +boton.dataset.id;
-            console.log("id del producto", idProducto);
-        })
+            const producto = bd.registrosPorId(idProducto);
+            console.log (producto);
+            carrito.agregar(producto);
+        });
     }
 }
 
